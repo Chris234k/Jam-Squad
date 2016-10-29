b078b339c6fbabbd78699f5dafdb4c0af7493217
Marty-PC\Marty <martin.wong.512@gmail.com>
2016-10-28 20:09:04 -0400

    Uploading some intial playercontroller and attachableparts scripts.

---
+++ Assets/Jam Squad/Scripts/IAttachablePart.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IAttachablePart
+{
+    void Initalize(KeyCode keyToActivate);
+    void ActivatePart();
+    void DeactivatePart();
+}
