bf13a9b88768a251cc2cb2117a793e12d53530b1
Chris Bruno <Chris@petricoregames.com>
2016-10-28 21:28:12 -0400

    Simple debris obstacles, going to experiment with spawning these in fields

---
+++ Assets/Jam Squad/Scripts/Obstacles/DebrisObstacle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebrisObstacle : Obstacle
+{
+    // Use this for initialization
+    void Start()
+    {
+        float ySpinRate = Random.Range(0, 20);
+        float zSpinRate = Random.Range(0, 20);
+
+        spinRate = new Vector3(0, ySpinRate, zSpinRate);
+    }
+
+    void Update()
+    {
+        SpinUpdate();
+    }
+}
