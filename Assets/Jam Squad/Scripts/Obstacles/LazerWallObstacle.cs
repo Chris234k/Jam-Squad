1f77e3e190773f36521cb50fa509f722509ce18e
Chris Bruno <Chris@petricoregames.com>
2016-10-28 22:57:01 -0400

    WIP lazer wall, committing to change to my desktop

---
+++ Assets/Jam Squad/Scripts/Obstacles/LazerWallObstacle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerWallObstacle : Obstacle
+{
+    public GameObject nodeA;
+    public GameObject nodeB;
+
+    public float spawnRadius;
+
+    void Start()
+    {
+        WasSpawned(null);
+    }
+
+    public override void WasSpawned(Spawner spawner)
+    {
+        nodeA.transform.position = GetPointInSphere(spawnRadius, transform.position);
+        nodeB.transform.position = GetPointInSphere(spawnRadius, transform.position);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
+}
