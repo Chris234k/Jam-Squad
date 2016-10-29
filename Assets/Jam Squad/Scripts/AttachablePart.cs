e023a1ac4192bfaf2e4d66326f0079f80af50860
Chris Bruno <Chris234k@gmail.com>
2016-10-29 15:38:14 -0400

    Commenting out collision debug info

--- Assets/Jam Squad/Scripts/AttachablePart.cs
+++ Assets/Jam Squad/Scripts/AttachablePart.cs
@@ -79,7 +79,7 @@ public class AttachablePart : MonoBehaviour
         // Collides with ObstacleWall or Enemy
         if (collision.gameObject.layer == 9 || collision.gameObject.layer == 11)
         {
-            Debug.Log("COLLISION VEL: " + collision.relativeVelocity.magnitude);
+            //Debug.Log("COLLISION VEL: " + collision.relativeVelocity.magnitude);
             //TODO Need to find appropriate factors to justify breakage
             if (!unbreakable && collision.relativeVelocity.magnitude >= COLLISION_BREAK_VELOCITY)
             {
