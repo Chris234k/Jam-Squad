df0d271f2b53cdaa16f8d8753625563e6251655b
Neil Sveri <nxsveri@gmail.com>
2016-10-28 21:10:30 -0400

    A bunch of stuff! Descriptive commit message!

---
+++ Assets/Jam Squad/Scripts/AttachablePart.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AttachablePart : MonoBehaviour, IAttachablePart {
+
+	public static event Action<AttachablePart> OnJointBreak;
+
+	public Rigidbody rigidbody;
+
+	private FixedJoint fixedJoint;
+
+	private AttachablePart connectedPart;
+
+	public void Initialize(AttachablePart argConnectedPart)
+	{
+		connectedPart 				= argConnectedPart;
+		fixedJoint 					= this.gameObject.AddComponent<FixedJoint> ();
+		fixedJoint.connectedBody 	= connectedPart.rigidbody;
+
+		OnJointBreak += HandleOnJointBreak;
+	}
+
+	void HandleOnJointBreak (AttachablePart argBrokenPart)
+	{
+		if (argBrokenPart == connectedPart)
+		{
+			OnJointBreak -= HandleOnJointBreak;
+			Destroy (fixedJoint);
+		}
+	}
+	
+	// Update is called once per frame
+	//	void Update () {
+	//		
+	//	}
+}
---
+++ Assets/Jam Squad/Scripts/PartAttachment.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartAttachment : MonoBehaviour {
+
+	[SerializeField]
+	private AttachablePart part;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast (transform.position, transform.forward, out hit,1000f))
+			{
+				AttachablePart newPart 		= GameObject.Instantiate<AttachablePart> (part);
+				newPart.transform.position 	= hit.point;
+				newPart.transform.forward 	= hit.normal;
+
+
+			}
+		}
+	}
+}
