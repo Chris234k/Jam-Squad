e0f35cfeccf019484cf92763d236ce7e6c8a8b16
chadrc <chadrcollins91@gmail.com>
2016-10-28 20:27:04 -0400

    Base Spawnable classes

---
+++ Assets/Jam Squad/Scripts/Spawning/SpawnableBehavior.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SpawnableBehavior : MonoBehaviour {
+	public abstract Vector3 Position {set;}
+}
---
+++ Assets/Jam Squad/Scripts/Spawning/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+	[System.Serializable]
+	private Transform spawnPoint;
+
+	[System.Serializable]
+	private float spawnInterval;
+
+	[System.Serializable]
+	private List<SpawnableBehavior> spawnables;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	IEnumerator spawnRoutine() {
+
+	}
+}
