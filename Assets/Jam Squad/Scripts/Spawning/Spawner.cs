717f35c047f9d22a0eb4bcef0e9c1d3d2f4c6bac
chadrc <chadrcollins91@gmail.com>
2016-10-28 21:21:30 -0400

    Created test spawning behavior Fleshed out spawner

--- Assets/Jam Squad/Scripts/Spawning/Spawner.cs
+++ Assets/Jam Squad/Scripts/Spawning/Spawner.cs
@@ -8,6 +8,9 @@ public class Spawner : MonoBehaviour {
 	private Transform spawnPoint;
 
 	[SerializeField]
+	private float spawnRadius;
+
+	[SerializeField]
 	private float spawnInterval;
 
 	[SerializeField]
@@ -15,11 +18,35 @@ public class Spawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		if (this.spawnPoint == null) {
+			this.spawnPoint = this.transform;
+		}
+		spawn ();
 	}
 
 	IEnumerator spawnRoutine() {
 
 		return null;
+	}
+
+	protected void spawn() {
+		if (spawnables.Count == 0) {
+			Debug.LogError ("No Spawnables in " + this.name);
+		}
+
+		int roll = Random.Range (0, spawnables.Count);
+		GameObject obj = GameObject.Instantiate (spawnables [roll].gameObject);
+		SpawnableBehavior spawnable = obj.GetComponent<SpawnableBehavior> ();
+		float distFromSpawnPoint = Random.Range (0, spawnRadius);
+		float angle = Random.Range (0, 360);
+		float x = Mathf.Cos (angle) * distFromSpawnPoint;
+		float z = Mathf.Sin (angle) * distFromSpawnPoint;
+		Vector3 position = new Vector3 (x, spawnPoint.transform.position.y, z);
+		spawnable.Position = position;
+		spawnable.Destroyed += onSpawnableDestroyed;
+	}
+
+	private void onSpawnableDestroyed(SpawnableBehavior spawnable) {
+		spawn ();
 	}
 }
---
+++ Assets/Jam Squad/Scripts/Spawning/TestSpawnable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSpawnable : SpawnableBehavior {
+
+	void Start() {
+		Invoke ("Destroy", 2.0f);
+	}
+
+	void Destroy() {
+		this.raiseDestroyed ();
+		GameObject.Destroy (this.gameObject);
+	}
+
+	#region implemented abstract members of SpawnableBehavior
+
+	public override Vector3 Position {
+		set {
+			this.transform.position = value;
+		}
+	}
+
+	#endregion
+}
