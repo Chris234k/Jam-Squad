c9709cee35502cf853c34d31650fb7c5614c6a4e
Chris Bruno <Chris@petricoregames.com>
2016-10-28 21:27:32 -0400

    Simple ring obstacles

---
+++ Assets/Jam Squad/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : SpawnableBehavior
+{
+    public Vector3 spinRate;
+
+    public override Vector3 Position
+    {
+        set
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    protected void SpinUpdate()
+    {
+        transform.eulerAngles += spinRate * Time.deltaTime;
+    }
+}
---
+++ Assets/Jam Squad/Scripts/Obstacles/RingObstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingObstacle : Obstacle
+{
+    void Update()
+    {
+        SpinUpdate();
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        // Speed boost player
+    }
+}
